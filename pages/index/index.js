850f10d4dae5406d0365d93aa8126974bd0259a5
GaoYang <gy148485@163.com>
2019-04-12 17:55:26 +0800

    完成首页静态图

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,26 @@ const API = require("../../wxApi/main.js");
 const util = require("../../utils/util.js");
 
 Page({
-
+  data:{
+    indexPage: [1, 2, 3, 4, 5, 6, 7],
+    current:0
+  },
+  addCurrent(e){
+    // 获取indexPage的页面个数
+    let count = this.data.indexPage.length;
+    if(this.data.current<count-1){
+      this.setData({
+        current: ++this.data.current
+      })
+    }
+    console.log(e);
+  },
+  reduceCurrent(e){
+    if (this.data.current > 0){
+      this.setData({
+        current: --this.data.current
+      })
+    }
+  }
 })
 
